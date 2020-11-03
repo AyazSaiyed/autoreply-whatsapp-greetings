a4bb43d3c0cae366bb27bdb2ebd7b6631d36e49f
Ayaz Saiyed <49748587+AyazSaiyed@users.noreply.github.com>
2020-11-03 14:16:13 +0000

    Update views.py

--- autoreplyapp/views.py
+++ autoreplyapp/views.py
@@ -1,7 +1,4 @@
-# /Users/yudiz/Downloads/MyExtraStuff/autoreplygreetings/autoreplyweb
-# (base) Ayaz:autoreplyweb yudiz$ 
-
-# https://dashboard.heroku.com/apps/autoreply-greetings/deploy/heroku-git
+#------ ------ ------ #AyazSaiyed  #November2020  #https://ayazsaiyed.ml ------ ------ ------#
 
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
@@ -18,30 +15,26 @@ from selenium import webdriver
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Create your views here.
-
 
 
 def index(requests):
 	return render(requests,'autoreplyapp/AutoResponMessages.html')
 
 def autoreplying(requests):
-	print(" Function Called ")
 
 	browser = webdriver.Chrome(ChromeDriverManager().install())
-	# browser = webdriver.Chrome('/Users/yudiz/Downloads/chromedriver')
 
 	browser.get('https://web.whatsapp.com/')
+	print( " ----- ")
+	print(" Opening the whatsapp web ")
+	print( " ----- ")
 	time.sleep(7)
 	letsdoit(browser)
-	return HttpResponse(" Messeages Sent ")
-	# return render(requests,'autoreplyapp/sendmessages.html')
+	return render(requests,'autoreplyapp/sendmessages.html')
 	
 
 def letsdoit(browser):
-	# time.sleep(6)
 
-	#Find the list of all names in the chat section
 	# browser.execute_script("window.scrollTo(0,500);")
 	try:
 		titles = browser.find_elements_by_class_name("_3CneP")
@@ -50,35 +43,20 @@ def letsdoit(browser):
 		k = 0
 		for info in titles:
 			k = k+1
-			# print("value ",k)
 			names_list.append(info.text)
-			# print("names",names_list)
-
-	
-	# 	# print(" In except")
-	# 	initial = 800
-	# 	for i in range(0, 2):
-	# 		browser.execute_script("document.getElementById('pane-side').scrollTop={}".format(initial))
-	# 		titles = browser.find_elements_by_class_name("_3CneP")
-	# 		names_list = []
-	# 		for info in titles:
-	# 			names_list.append(info.text)
-	# 			# print("names",names_list)
-	# 	initial+=10
 	except:
 		pass
 
-	print(" Names ",names_list)
+	# print(" Names ",names_list)
+	
 	for name in names_list:
 	    try:
-	        #Open the chat of a specific person
 	        person = browser.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 
 	        print("Person Name",name)
 	        person.click()
 	        # _357i8
 	        
-	        # browser.execute_script("window.scrollTo(0,1);")
 	        
 	        #Get the list of messages exchanged with the person
 	        message_list = browser.find_elements_by_css_selector("span.selectable-text.invisible-space.copyable-text")
@@ -88,16 +66,14 @@ def letsdoit(browser):
 
 	        #Checking the last message
 	        if "happy" in messages[-1].lower() and "diwali" in messages[-1].lower():
-	            print("Wish found")
+	            print("Greeting found from "+name)
 	            #Navigate to the type message here section
 	            message_box = browser.find_element_by_xpath('//div[@class="_3uMse"]')
 	            msg = "Thank you "+name+" ☺️ for wishes and a very Happiee diwalii to you too and your family"
 	            time.sleep(0.2)
-	            #Typing message in that section
 	            message_box.send_keys(msg)
-	            #Accessing the send button
 	            message_button = browser.find_element_by_xpath('//button[@class="_1U1xa"]')
-	            #Clicking the send button
+	            #Let's send
 	            message_button.click()
 
 	    except:
@@ -105,7 +81,6 @@ def letsdoit(browser):
 	        continue
 	# browser.close()
 
-	return HttpResponse(" Messeages Sent ")
+	return HttpResponse(" Messeages are Sent ")
 
 
-# autoreplya(requests)
